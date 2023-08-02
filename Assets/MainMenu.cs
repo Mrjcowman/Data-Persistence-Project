8d823086acd9a0e6b5b0ee0e3357765f22dd1d5f
Jacob Cowan <Mrjcowman@gmail.com>
2023-08-02 13:34:07 -0700

    Add main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void OnExitButtonPressed()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
